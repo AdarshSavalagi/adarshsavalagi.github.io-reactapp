cf03f3a96e1dc348af6aff2c1e1be2bc05e537a9
ADARSH <120472249+AdarshSavalagi@users.noreply.github.com>
2023-11-01 13:02:49 +0530

    loading animation added

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,54 +1,57 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import location from '../assets/location.svg'
 import axios from 'axios';
 axios.defaults.withCredentials = true;
-function sendPostRequest(formData) {
-  axios.post('https://alumni.aldoiris.online/api-business/contacts', formData)
-    .then((response) => {
-      // Handle the response if needed
-      console.log('Response:', response.data);
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Error:', error);
-    });
-}
+
 
 
 export default function Contact() {
-    const [visible ,setVisible]=useState([]);
-    useEffect(()=>{
-        const handleScroll = ()=>{
+    const sendPostRequest=(formData)=> {
+        axios.post('https://alumni.aldoiris.online/api-business/contacts', formData)
+            .then((response) => {
+                // Handle the response if needed
+                setLoading(false);
+                console.log('Response:', response.data);
+            })
+            .catch((error) => {
+                // Handle errors
+                setLoading(false);
+                console.error('Error:', error);
+            });
+    }
+    const [visible, setVisible] = useState([]);
+    useEffect(() => {
+        const handleScroll = () => {
             const cardElements = document.querySelectorAll('.contact-items');
             const viewportHeight = window.innerHeight;
-            const threshold=viewportHeight*0.8;
-            const newVisibleCards=Array.from(cardElements).map((cardElement)=>{
+            const threshold = viewportHeight * 0.8;
+            const newVisibleCards = Array.from(cardElements).map((cardElement) => {
                 const cardTop = cardElement.getBoundingClientRect().top;
-                return cardTop<threshold;
+                return cardTop < threshold;
             });
             setVisible(newVisibleCards);
         };
-        window.addEventListener('scroll',handleScroll);
-        return ()=>{
-            window.removeEventListener('scroll',handleScroll)
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
         };
     });
     // for sending post request 
-    const [loading, setLoading]=useState([false,false]);
-    const [formData, setFormData]=useState({
-        'name':'','email':'','message':''
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        'name': '', 'email': '', 'message': ''
     });
-    const handleInputChange=(e)=>{
+    const handleInputChange = (e) => {
         const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+        setFormData({ ...formData, [name]: value });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+        setLoading(true);
         // illi form na send mado logic bartade
         sendPostRequest(formData);
-      };
+    };
     return (
         <>
             <div className='container mt-5  justify-center mx-auto'>
@@ -60,7 +63,7 @@ export default function Contact() {
                     {/* cards */}
                     <div className="container px-5  mx-auto">
                         <div className="grid md:grid-rows-2 ">
-                            <div className={`p-2 contact-items ${visible[0]?'visible':''}`}>
+                            <div className={`p-2 contact-items ${visible[0] ? 'visible' : ''}`}>
                                 <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col hover:shadow ">
                                     <div className="flex items-center mb-3">
                                         <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-600 text-white flex-shrink-0">
@@ -75,7 +78,7 @@ export default function Contact() {
                                 </div>
                             </div>
                             <div className='md:columns-2'>
-                                <div className={`p-2 contact-items ${visible[1]?'visible':''}`}>
+                                <div className={`p-2 contact-items ${visible[1] ? 'visible' : ''}`}>
                                     <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col hover:shadow ">
                                         <div className="flex items-center mb-3">
                                             <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-600 text-white flex-shrink-0">
@@ -89,7 +92,7 @@ export default function Contact() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className={`p-2 contact-items ${visible[2]?'visible':''}`}>
+                                <div className={`p-2 contact-items ${visible[2] ? 'visible' : ''}`}>
                                     <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col hover:shadow ">
                                         <div className="flex items-center mb-3">
                                             <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-orange-600 text-white flex-shrink-0">
@@ -107,7 +110,7 @@ export default function Contact() {
                         </div>
                     </div>
                     {/* form */}
-                    <div className={`container contact-items ${visible[3]?'visible':''}`}>
+                    <div className={`container contact-items ${visible[3] ? 'visible' : ''}`}>
                         <form onSubmit={handleSubmit}>
                             <div className="  flex flex-wrap mx-3 mb-6 ">
                                 <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0' >
@@ -124,7 +127,14 @@ export default function Contact() {
                                 <div className='w-full px-3' >
                                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
                                     <textarea id="message" name='message' rows="4" value={formData.message} onChange={handleInputChange} className="border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block  p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-orange-500 dark:focus:border-orange-500  w-full" placeholder="Write your thoughts here..."></textarea>
-                                    <button type="submit" className="text-white  bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 my-2">Submit</button>
+                                    <button type="submit" className={`${!loading ? '' : 'hidden'} bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 my-2  text-white`}>Submit</button>
+                                    <button disabled type="button" className={`${loading ? '' : 'hidden'} bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 my-2  text-white`}>
+                                        <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                                        </svg>
+                                        Submitting...
+                                    </button>
                                 </div>
                             </div>
                         </form>
